383717d3d7d32e11560932fff7ac604cca9d599a
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-03-29 11:45:14 -0400

    Inital COmmit

---
+++ MathFacts/Addition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFacts
+{
+    class Addition
+    {
+        public void AdditionTable()
+        { 
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            string appTitle = @"
+  ______         __        __  __    __      __                            ________            __        __           
+ /      \       /  |      /  |/  |  /  |    /  |                          /        |          /  |      /  |          
+/$$$$$$  |  ____$$ |  ____$$ |$$/  _$$ |_   $$/   ______   _______        $$$$$$$$/   ______  $$ |____  $$ |  ______  
+$$ |__$$ | /    $$ | /    $$ |/  |/ $$   |  /  | /      \ /       \          $$ |    /      \ $$      \ $$ | /      \ 
+$$    $$ |/$$$$$$$ |/$$$$$$$ |$$ |$$$$$$/   $$ |/$$$$$$  |$$$$$$$  |         $$ |    $$$$$$  |$$$$$$$  |$$ |/$$$$$$  |
+$$$$$$$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ | __ $$ |$$ |  $$ |$$ |  $$ |         $$ |    /    $$ |$$ |  $$ |$$ |$$    $$ |
+$$ |  $$ |$$ \__$$ |$$ \__$$ |$$ |  $$ |/  |$$ |$$ \__$$ |$$ |  $$ |         $$ |   /$$$$$$$ |$$ |__$$ |$$ |$$$$$$$$/ 
+$$ |  $$ |$$    $$ |$$    $$ |$$ |  $$  $$/ $$ |$$    $$/ $$ |  $$ |         $$ |   $$    $$ |$$    $$/ $$ |$$       |
+$$/   $$/  $$$$$$$/  $$$$$$$/ $$/    $$$$/  $$/  $$$$$$/  $$/   $$/          $$/     $$$$$$$/ $$$$$$$/  $$/  $$$$$$$/ ";
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("You Will be able to sleect numbers 1 - 10");
+            Console.WriteLine("Next you will see the numbers between these selections be added");
+            Console.WriteLine(" to numbrrs 1 - 10");
+            Console.WriteLine("");
+            Console.WriteLine("Select a starting number 1 and 10");
+        }
+
+        public void AdditionSheet(int startNum, int endNum)
+        {
+            //TODO - build out logic for add table
+            for (int i = startNum - 1; i <= endNum; i++)
+            {
+                if (i == startNum - 1)
+                {
+                    Console.Write(String.Format("{0, 6}", "n"));
+                }
+                else
+                {
+                    Console.Write(String.Format("{0, 6}", i));
+                }
+            }
+            Console.WriteLine("\n");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.Write(String.Format("{0, 6}", i));
+                    for (int n = startNum; n <= endNum; n++)
+                    {
+                    string output = String.Format("{0, 6}", i + n);
+                    Console.Write(output);
+                    }
+                Console.WriteLine("");
+               
+
+                }
+            Console.ReadLine();
+
+        }
+    }
+
+    
+}
---
+++ MathFacts/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Math Facts App";
+            int choice = 0;
+            Addition addTable = new Addition();
+            Multiply multTable = new Multiply();
+            do
+            {
+                AppTitle();
+                choice = MainMenu();
+                //TODO - HAndle Addition CHoice
+                if (choice == 1)
+                {
+                    bool continueChoice = true;
+                    int startNum = 0;
+                    int endNum = 0;
+                    do
+                    {
+                        RunAddition(addTable, ref continueChoice, ref startNum, ref endNum);
+                    } while (continueChoice == true);
+
+                    
+                }
+
+                if (choice == 2)
+                {
+                    bool continueChoice = true;
+                    int startNum = 0;
+                    int endNum = 0;
+                    do
+                    {
+                        RunMult(multTable, ref continueChoice, ref startNum, ref endNum);
+                    } while (continueChoice == true);
+                }
+
+            } while (choice != 3);
+        }
+
+        private static void RunAddition(Addition addTable, ref bool continueChoice, ref int startNum, ref int endNum)
+        {
+            addTable.AdditionTable();
+            try
+            {
+                Console.WriteLine("Enter your starting number: ");
+                startNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your ending number: ");
+                endNum = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid number");
+                Console.ReadLine();
+
+            }
+            finally
+            {
+                //TODO validate
+                if (startNum > 0 && endNum <= 10)
+                {
+                    addTable.AdditionTable();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("Your addition table for {0} - {1}", startNum, endNum);
+                    addTable.AdditionSheet(startNum, endNum);
+
+                    Console.WriteLine("");
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Would you like more additions facts [y/n]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        string moreFacts = Console.ReadLine();
+                        if (moreFacts == "y")
+                        {
+                            continueChoice = true;
+
+                        }
+                        else if (moreFacts == "n")
+                        {
+                            continueChoice = false;
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+
+
+                //handle addtiion table
+                //logic to allow contine/end additions
+            }
+        }
+
+        private static void RunMult(Multiply multTable, ref bool continueChoice, ref int startNum, ref int endNum)
+        {
+            multTable.MultTable();
+            try
+            {
+                Console.WriteLine("Enter your starting number: ");
+                startNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your ending number: ");
+                endNum = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid number");
+                Console.ReadLine();
+
+            }
+            finally
+            {
+                //TODO validate
+                if (startNum > 0 && endNum <= 10)
+                {
+                    multTable.MultTable();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("Your addition table for {0} - {1}", startNum, endNum);
+                    multTable.MultSheet(startNum, endNum);
+
+                    Console.WriteLine("");
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Would you like more multiplication facts [y/n]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        string moreFacts = Console.ReadLine();
+                        if (moreFacts == "y")
+                        {
+                            continueChoice = true;
+
+                        }
+                        else if (moreFacts == "n")
+                        {
+                            continueChoice = false;
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+
+
+                //handle addtiion table
+                //logic to allow contine/end additions
+            }
+        }
+
+        private static int MainMenu()
+        {
+            int choice;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("----------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Option 1: Addition Facts");
+            Console.WriteLine("Option 2: Multiplication Facts");
+            Console.WriteLine("Option 3 Exit Program");
+            //TODO - Fic Exception handleing
+            choice = Int32.Parse(Console.ReadLine());
+            return choice;
+        }
+
+        private static void AppTitle()
+        {
+            Console.Clear();
+            string appTitle = @"
+ __  __       _   _       _____          _            _                
+|  \/  | __ _| |_| |__   |  ___|_ _  ___| |_ ____    / \   _ __  _ __  
+| |\/| |/ _` | __| '_ \  | |_ / _` |/ __| __|_  /   / _ \ | '_ \| '_ \ 
+| |  | | (_| | |_| | | | |  _| (_| | (__| |_ / /   / ___ \| |_) | |_) |
+|_|  |_|\__,_|\__|_| |_| |_|  \__,_|\___|\__/___| /_/   \_\ .__/| .__/ 
+                                                          |_|   |_|    ";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(appTitle);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
